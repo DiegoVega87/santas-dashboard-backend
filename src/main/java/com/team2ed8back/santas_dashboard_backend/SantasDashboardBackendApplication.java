c8e43dcd9514850f77c6330ccb83ee2c2fd4f9a7
Diego Vega <diegovega@me.com>
2024-11-05 10:29:45 +0100

    Initial commit: Create repo with main branch

---
+++ src/main/java/com/team2ed8back/santas_dashboard_backend/SantasDashboardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.team2ed8back.santas_dashboard_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantasDashboardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantasDashboardBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team2ed8back/santas_dashboard_backend/SantasDashboardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team2ed8back.santas_dashboard_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantasDashboardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
