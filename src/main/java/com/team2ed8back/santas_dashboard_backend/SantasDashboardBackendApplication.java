4890a2c07f26a9f54849b157978f30b12ba3f6a4
Diego Vega <diegovega@me.com>
2024-11-26 10:31:20 +0100

    hotfix: added code to allow connections to the api from any ip

--- src/main/java/com/team2ed8back/santas_dashboard_backend/SantasDashboardBackendApplication.java
+++ src/main/java/com/team2ed8back/santas_dashboard_backend/SantasDashboardBackendApplication.java
@@ -4,6 +4,10 @@ import com.team2ed8back.santas_dashboard_backend.controller.ChristmasLetterContr
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SantasDashboardBackendApplication {
@@ -12,6 +16,20 @@ public class SantasDashboardBackendApplication {
 		ApplicationContext context = SpringApplication.run(SantasDashboardBackendApplication.class, args);
 		ChristmasLetterController christmasLetterController = context.getBean(ChristmasLetterController.class);
 		christmasLetterController.insertChristmasLetters();
+	}
+
+	@Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
 	}
 
 }
