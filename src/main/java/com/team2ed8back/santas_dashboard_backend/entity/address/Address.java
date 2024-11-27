c6b84be61b3eb920403d81d2b1ee494053380a32
Diego Vega <diegovega@me.com>
2024-11-27 14:53:08 +0100

    fix: added supressed lombok. Posible preventing address beans to be created

--- src/main/java/com/team2ed8back/santas_dashboard_backend/entity/address/Address.java
+++ src/main/java/com/team2ed8back/santas_dashboard_backend/entity/address/Address.java
@@ -5,10 +5,9 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+@CrossOrigin("*")
 @Entity
 @Table(name = "address")
 public class Address {
@@ -24,4 +23,61 @@ public class Address {
     @Column(name = "lng", nullable = false)
     private long longitude;
 
+    public Address() {
+    }
+
+    public Address(long id, String name, String address, long latitude, long longitude) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public Address(String name, String address, long latitude, long longitude) {
+        this.name = name;
+        this.address = address;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public long getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(long latitude) {
+        this.latitude = latitude;
+    }
+
+    public long getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(long longitude) {
+        this.longitude = longitude;
+    }
 }
