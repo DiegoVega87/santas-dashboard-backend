ba858f6e7e0d474c8e226280c347b8815c4635df
Diego Vega <diegovega@me.com>
2024-11-28 11:00:49 +0100

    perf: Add Swagger configuration for Santas Dashboard API

---
+++ src/main/java/com/team2ed8back/santas_dashboard_backend/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.team2ed8back.santas_dashboard_backend.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("https://vivacious-elegance-production.up.railway.app/"))
+                .info(new Info().title("Santas Dashboard API")
+                        .version("1.0")
+                        .description("API for Santas Dashboard"));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public-api")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
