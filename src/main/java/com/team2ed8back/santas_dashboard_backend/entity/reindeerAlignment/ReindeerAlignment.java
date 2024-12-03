36b91b03fe0c0127d38c63d7f89d7c97a3d45430
Diego Vega <diegovega@me.com>
2024-12-03 20:57:18 +0100

    fix: change datatypes for latitude and longitude

--- src/main/java/com/team2ed8back/santas_dashboard_backend/entity/reindeerAlignment/ReindeerAlignment.java
+++ src/main/java/com/team2ed8back/santas_dashboard_backend/entity/reindeerAlignment/ReindeerAlignment.java
@@ -7,11 +7,7 @@ import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "reindeer_alignment", uniqueConstraints = {
-        @UniqueConstraint(columnNames = {"front1", "front2", "front3",
-                "middle1", "middle2", "middle3",
-                "back1", "back2", "back3"})
-})
+@Table(name = "reindeer_alignment")
 public class ReindeerAlignment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
