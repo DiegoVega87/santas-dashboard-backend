5fc529b1296896224c866a04328ac6e458a5d55f
juanjose999 <juanjose000012@gmail.com>
2024-12-07 09:37:33 -0500

    fix: Add return of the method.

--- src/main/java/com/team2ed8back/santas_dashboard_backend/controller/ChildController.java
+++ src/main/java/com/team2ed8back/santas_dashboard_backend/controller/ChildController.java
@@ -64,7 +64,7 @@ public class ChildController {
     }
 
     @DeleteMapping
-    public ResponseEntity<?> deleteAllChilds() {
+    public void deleteAllChilds() {
         childService.delete();
     }
 }
