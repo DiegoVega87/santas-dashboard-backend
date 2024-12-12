77a12e7a2e1b9004817e719ab0ab88d9203d1097
Diego Vega <diegovega@me.com>
2024-12-12 14:41:55 +0100

    perf: cleanup unsed code

--- src/main/java/com/team2ed8back/santas_dashboard_backend/service/reindeer/ReindeerAlignmentService.java
+++ src/main/java/com/team2ed8back/santas_dashboard_backend/service/reindeer/ReindeerAlignmentService.java
@@ -13,10 +13,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+
 import java.util.stream.Collectors;
 
 @Service
