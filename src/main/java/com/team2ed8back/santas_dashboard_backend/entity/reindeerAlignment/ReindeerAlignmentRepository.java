177b86c99dad718f7c7aaca0fa91f39b483d1d82
Diego Vega <diegovega@me.com>
2024-12-17 11:24:50 +0100

    fix: udate alignments

--- src/main/java/com/team2ed8back/santas_dashboard_backend/entity/reindeerAlignment/ReindeerAlignmentRepository.java
+++ src/main/java/com/team2ed8back/santas_dashboard_backend/entity/reindeerAlignment/ReindeerAlignmentRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ReindeerAlignmentRepository extends JpaRepository<ReindeerAlignment, Long> {
 
     ReindeerAlignment findByName(String snowyAlignment);
+
+    ReindeerAlignment save(ReindeerAlignment existingAlignment, ReindeerAlignment alignment);
 }
